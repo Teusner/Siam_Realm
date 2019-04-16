e1ae76a5df6ba27e184a9a7cd77b332e80bacf4a
Teusner <quentin.bratea@ensta-bretagne.org>
2019-04-16 21:45:54 +0200

    Implementing the Direction module, bearing and scalar methods

--- Module/Direction.py
+++ Module/Direction.py
@@ -1,9 +1,30 @@
 import numpy as np
 
-class Direction(np.array) :
-    def __init__(self, coords):
-        if np.linalg.norm(np.array(coords)) != 1 and (coords[0] != 0 or coords[1] != 0) :
-            coords == (0, 1)
-        self = np.array(coords)
 
+class Direction(np.ndarray):
+    def __new__(cls, data):
+        if np.linalg.norm(np.array(data)) == 1 and (data[0] != 0 or data[1] != 0):
+            dir = np.asarray(data).view(cls)
+            return dir
+        else:
+            return None
 
+    def __init__(self, data):
+        self.angle = np.arccos(np.array([1, 0]) @ self)
+
+    def bearing(self, other):
+        return other @ self
+
+    def scalar(self, others):
+        result = 0
+        is_negative = False
+        for other in others:
+            result += self.bearing(other)
+            if result < 0:
+                is_negative = True
+                break
+        return int(result), is_negative
+
+if __name__ == '__main__':
+    a = Direction([1, 0])
+    print(a.scalar([np.array([-1, 0])]))
