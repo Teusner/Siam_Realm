cfeb9f07bb16251408908bb7b63f2290aa6b0314
Teusner <quentin.bratea@ensta-bretagne.org>
2019-04-29 10:34:17 +0200

    adding __init__.py

---
+++ Module/__init__.py
@@ -0,0 +1,23 @@
+# -*-coding:utf-8 -*-
+
+"""
+        * * * Module KingOfSiam* * *
+
+Ce module contient 1 sous modules :
+        * KingOfSiam
+
+je vous passe le blabla
+
+"""
+
+# info
+__version__ = "1.0"
+__author__ = "moi  ;)"
+__date__ = "vendredi 22 avril 2011"
+
+# import sub modules
+import Module
+from Module.KingOfSiam import Animal
+from Module.KingOfSiam import GameMap
+from Module.KingOfSiam import Boulder
+from Module.KingOfSiam import Cross
